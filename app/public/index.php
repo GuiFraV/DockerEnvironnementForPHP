689fbb3baa6860a44ee48016fadd721bd9a330bb
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-05 17:53:11 +0100

    setup mysql db

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,8 @@
 <?php
+    $pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-phpinfo();
+    $query = $pdo->query('SHOW VARIABLES like "version"');
+
+    $row = $query->fetch();
+
+    echo 'MySQL version:' . $row['Value'];
